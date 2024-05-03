397c95c8ecf50a799a1b85014a0de906fffea033
kanta_fuchigami <kanta_fuchigami@serio.inc>
2024-05-03 15:06:09 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({ extended : false}));
+app.use(express.static(path.join(__dirname, "public")))
+
+// app.get("/", (req, res) => {
+//     res.send("<h1>Top page</h1>");
+// });
+
+// app.get("/about", (req, res) => {
+//     res.send("About page");
+// });
+
+app.post("/api/v1/quiz", (req, res) => {
+    const answer = req.body.answer;
+    res.send(answer);
+});
+
+app.listen(3000, () => {
+    console.log("I am running.");
+});
